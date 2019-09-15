fd62b086031934ea7cf261e900071b934009e341
Amir Tamimi <amir.tamim@trojans.dsu.edu>
2019-09-14 22:22:22 -0700

    Javascript for current class

--- dist/js/smoothscroll.js
+++ dist/js/smoothscroll.js
@@ -6,3 +6,8 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+//current class
+document.getElementById('main-nav').addEventListener('click', function(e) {
+  e.target.classList.add('current');
+})
